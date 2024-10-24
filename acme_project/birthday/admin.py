0e6d59b9d19485d0817b925671fb203a7a909cec
Губарев <nikita.gv@yandex.ru>
2024-10-24 12:52:33 +0300

    с правками от 24.10

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,22 @@
 from django.contrib import admin
+
+
+from .models import Birthday
+
+class BirthdayAdmin(admin.ModelAdmin):
+    """Создаем модель Birthday."""
+    list_display = (
+        'id',
+        'first_name',
+        'last_name',
+        'birthday'
+    )
+
+    list_editable = (
+        'first_name',
+        'last_name',
+        'birthday'
+    )
+
+
+admin.site.register(Birthday, BirthdayAdmin)
